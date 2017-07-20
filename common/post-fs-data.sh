3b3c8224f18e0a09d3904463de98e1096c86ff1b
Haruue Icymoon <haruue@caoyue.com.cn>
2017-07-21 02:24:26 +0800

    merge origin files

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -4,8 +4,22 @@
 MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
+
 # More info in the main Magisk thread
 cd $MODDIR/system/xbin
-tar xf symbol-links.tar
+
+# Merge origin files in /system/xbin
+echo "Merging origin /system/xbin..."
+for f in /system/xbin/*; do 
+    LINK=$(readlink $f)
+    if [ "$LINK" == "" -a "$f" != "/system/xbin/busybox" -o "$LINK" != "/system/xbin/busybox" -a "$f" != "/system/xbin/busybox" ]; then
+        echo "copying" "$f"
+        cp -a "$f" .
+    fi
+done
+
+# Create symbol links for busybox
+echo "Creating symbol-links..."
+tar xvf symbol-links.tar
 rm symbol-links.tar
-cd $OLDPWD
+
